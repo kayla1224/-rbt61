4e35fc14dfef6fec34f7c0073c334f6e663f9fac
kayla1224 <145270532+kayla1224@users.noreply.github.com>
2023-09-17 21:29:30 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showString("Kayla")
+    basic.pause(1000)
+    basic.showNumber(11)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+})
